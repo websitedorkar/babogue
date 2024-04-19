ec77a549c729bb982f2ed677f35e06883f70d873
Arif Hossin || Full Stack Developer <websitedorkar@gmail.com>
2024-04-19 09:12:58 +0600

    Update

--- src/app/(FrontPage)/InstagramFeed/index.tsx
+++ src/app/(FrontPage)/InstagramFeed/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import BG from './img/bg.png';
-import Image from 'next/image';
-import { feeddata } from './data.feed';
+import Image__1 from './img/1.png';
+import Image__2 from './img/2.png';
+import Image__3 from './img/3.png';
+import Image__4 from './img/4.png';
 
 const InstagramFeed = () => {
     return (
@@ -14,11 +16,18 @@ const InstagramFeed = () => {
             </div>
             <div className="container">
                 <div className="grid grid-cols-4 gap-4">
-                    {feeddata && feeddata.map(item => (
-                        <div key={item.id} className={`grid__item ${item.className ?? ''}`}>
-                            <Image src={item.thumbnail} alt={item.heading} />
-                        </div>
-                    ))}
+                    <div className="grid__item">
+                        <img src={Image__1.src} alt="Instagram" />
+                    </div>
+                    <div className="grid__item">
+                        <img src={Image__2.src} alt="Instagram" />
+                    </div>
+                    <div className="grid__item">
+                        <img src={Image__3.src} alt="Instagram" />
+                    </div>
+                    <div className="grid__item">
+                        <img src={Image__4.src} alt="Instagram" />
+                    </div>
                 </div>
             </div>
         </section>
