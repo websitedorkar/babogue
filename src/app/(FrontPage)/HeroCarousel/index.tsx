44830c22a235337c93c0ae3af885b0a05400298c
Arif Hossin || Full Stack Developer <166840312+websitedorkar@users.noreply.github.com>
2024-04-24 06:20:31 +0600

    Update index.tsx

--- src/app/(FrontPage)/HeroCarousel/index.tsx
+++ src/app/(FrontPage)/HeroCarousel/index.tsx
@@ -44,6 +44,7 @@ const HeroCarousel = () => {
                             modules={[EffectFade, Pagination, Autoplay]}
                             spaceBetween={40}
                             slidesPerView={1}
+                            loop
                             effect={"fade"}
                             fadeEffect= {{ crossFade: true }}
                             className='slide-equal-height'
@@ -61,47 +62,30 @@ const HeroCarousel = () => {
                                         {({ isActive }) => (
                                             <div className="container">
                                                 <div className="grid grid-cols-1 gap-y-[15px] lg:gap-y-[70px] lg:grid-cols-[0.75fr_1fr] items-end">
-                                                    <div className="slide__content px-[15px] md:px-0 pt-[25px] md:pt-[40px] xl:pt-[130px] lg:pb-[180px] lg:min-h-[70vh]">
-                                                        {slide.caption && <motion.h6                                                         
+                                                    <motion.div                                            
                                                         initial={{ y: 10, opacity: 0 }}
                                                         animate={{ y: isActive ? 0 : 10, opacity: isActive ? 1 : 0 }}
                                                         exit={{ y: -10, opacity: 0 }}
-                                                        transition={{ duration: 0.3, delay: 0.3 }}
+                                                        transition={{ duration: 0.3, delay: 0.3 }}                                                    
+                                                    className="slide__content px-[15px] md:px-0 pt-[25px] md:pt-[40px] xl:pt-[130px] lg:pb-[180px] lg:min-h-[70vh]">
+                                                        {slide.caption && <motion.h6
                                                         className='text-primary text-sm md:text-xl font-normal mb-2'>{slide.caption}</motion.h6>}
 
                                                         {slide.heading && <motion.h2
-                                                        initial={{ y: 10, opacity: 0 }}
-                                                        animate={{ y: isActive ? 0 : 10, opacity: isActive ? 1 : 0 }}
-                                                        exit={{ y: -10, opacity: 0 }}
-                                                        transition={{ duration: 0.3, delay: 0.6 }}
                                                         className='text-dark text-3xl md:text-5xl xl:text-6xl leading-[1.1] font-bold mb-4' dangerouslySetInnerHTML={{__html: slide.heading }}></motion.h2>}
 
                                                         {slide.list && <div className='text-dark flex flex-col gap-2 lg:gap-4 md:max-w-full'>{slide.list.map((item, i) => (
-                                                            <motion.li
-                                                            initial={{ y: 10, opacity: 0 }}
-                                                            animate={{ y: isActive ? 0 : 10, opacity: isActive ? 1 : 0 }}
-                                                            exit={{ y: -10, opacity: 0 }}
-                                                            transition={{ duration: 0.3, delay: 1+ i/5  }}
+                                                            <motion.li 
                                                             className='flex gap-4' key={i}>
                                                                 {item.icon && <span className='min-w-[18px] mt-3'><Image src={item.icon} alt="icon" /></span>}
                                                                 {item.text && <div className='text-sm md:text-base leading-5 lg:leading-6'>{item.text}</div>}
                                                             </motion.li>
                                                         ))}</div>}
-                                                        {slide.description && <motion.div 
-                                                            initial={{ y: 10, opacity: 0 }}
-                                                            animate={{ y: isActive ? 0 : 10, opacity: isActive ? 1 : 0 }}
-                                                            exit={{ y: -10, opacity: 0 }}
-                                                            transition={{ duration: 0.3, delay: 1.2 }}
-                                                        className='text-dark text-sm'>{slide.description}</motion.div>}
-                                                        {slide.button__label && <motion.div 
-                                                            initial={{ y: 10, opacity: 0 }}
-                                                            animate={{ y: isActive ? 0 : 10, opacity: isActive ? 1 : 0 }}
-                                                            exit={{ y: -10, opacity: 0 }}
-                                                            transition={{ duration: 0.3, delay: 1.4 }}
-                                                        className='pt-4'>
+                                                        {slide.description && <motion.div className='text-dark text-sm'>{slide.description}</motion.div>}
+                                                        {slide.button__label && <motion.div className='pt-4'>
                                                             <Link href={slide.button__url ?? '#'}><Button size={'sm'} className=''>{slide.button__label}</Button></Link>
                                                         </motion.div>}
-                                                    </div>
+                                                    </motion.div>
                                                     <div className="relative">
                                                         <motion.div className="slide__banner -ms-4 -me-10 md:mx-0"
                                                             initial={false}
